b2986e0cf1422adf1a21dafb20a3641e1e38b74a
JhullyKF <jhully.ferreira@cs.up.edu.br>
2025-09-10 20:03:49 -0300

    fix - add imagens da pagina de login e configura para exibição

--- SWF_frontend/src/app/app.ts
+++ SWF_frontend/src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { Login } from "./pages/login/login";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule],
+  imports: [RouterLink, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
