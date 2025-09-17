e3c39fed2ba9385f86c5eab9b37bc8d80809f872
JhullyKF <jhully.ferreira@cs.up.edu.br>
2025-09-16 22:01:22 -0300

    feat - add função de esconder senha

--- SWF_frontend/src/app/app.config.ts
+++ SWF_frontend/src/app/app.config.ts
@@ -1,22 +1,36 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, importProvidersFrom } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import pt from '@angular/common/locales/pt';
 
-import { routes } from './app.routes';
-import { icons } from './icons-provider';
+// Importações do NG-ZORRO
 import { provideNzIcons } from 'ng-zorro-antd/icon';
 import { pt_BR, provideNzI18n } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import pt from '@angular/common/locales/pt';
-import { FormsModule } from '@angular/forms';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient } from '@angular/common/http';
+import { EyeOutline, EyeInvisibleOutline } from "@ant-design/icons-angular/icons";
 
+import { routes } from './app.routes';
+
+// 1. Definição dos ícones que serão usados
+const appIcons = [
+  EyeOutline,
+  EyeInvisibleOutline
+];
+
+// 2. Registro do idioma português para a aplicação
 registerLocaleData(pt);
 
+// 3. ÚNICA declaração do appConfig com todos os providers corretos
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideNzIcons(icons), provideNzI18n(pt_BR), importProvidersFrom(FormsModule), provideAnimationsAsync(), provideHttpClient()
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(),
+    importProvidersFrom(FormsModule),
+    provideNzI18n(pt_BR),
+    // Forma moderna e correta de fornecer os ícones
+    provideNzIcons(appIcons)
   ]
 };
--- SWF_frontend/src/app/pages/login/login.ts
+++ SWF_frontend/src/app/pages/login/login.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from '../../app.routes';
+import { NzIconModule, NZ_ICONS } from 'ng-zorro-antd/icon';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { EyeOutline, EyeInvisibleOutline } from '@ant-design/icons-angular/icons';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [NzIconModule, FormsModule],
+  providers: [
+    { provide: NZ_ICONS, useValue: [EyeOutline, EyeInvisibleOutline] }
+  ],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrls: ['./login.css']
 })
 export class Login {
+  senha: String = '';
+  passwordVisible: boolean = false;
 
+  togglePassword(){
+    this.passwordVisible = !this.passwordVisible
+  }
+  onSubmit(): void {
+    console.log('Formulário enviado!');
+  }
 }
